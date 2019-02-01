3a5267370056537bcedeff86b123adcb3ccc1787
zefyr <zefyr@zezz.com>
2019-02-01 14:29:58 +0900

    [TKN-524] [TKN-523] [TKN-517] [TKN-525] DexKey API updated

--- src/main/java/io/colligence/talken/dex/api/dex/TxEncryptedData.java
+++ src/main/java/io/colligence/talken/dex/api/dex/TxEncryptedData.java
@@ -3,12 +3,12 @@ package io.colligence.talken.dex.api.dex;
 import ch.qos.logback.core.encoder.ByteArrayUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.colligence.talken.common.util.JSONWriter;
+import io.colligence.talken.common.util.RandomStringGenerator;
 import io.colligence.talken.dex.util.AES256Util;
 
 import javax.crypto.KeyGenerator;
 import java.security.GeneralSecurityException;
 import java.security.NoSuchAlgorithmException;
-import java.util.UUID;
 
 public class TxEncryptedData {
 	private TxInformation txInfo;
@@ -25,10 +25,8 @@ public class TxEncryptedData {
 			KeyGenerator keyGen = KeyGenerator.getInstance("AES");
 			keyGen.init(256);
 			key = ByteArrayUtil.toHexString(keyGen.generateKey().getEncoded()).substring(0, 16);
-//			key2 = ByteArrayUtil.toHexString(keyGen.generateKey().getEncoded());
 		} catch(NoSuchAlgorithmException e) {
-			key = UUID.randomUUID().toString().replaceAll("[^0-9a-zA-Z]", "").substring(0, 16);
-//			key2 = UUID.randomUUID().toString().replaceAll("[^0-9a-zA-Z]", "").toLowerCase().substring(0, 16);
+			key = RandomStringGenerator.generate(16);
 		}
 
 		encrypted = new AES256Util(key).encrypt(JSONWriter.toJsonString(txInfo));
