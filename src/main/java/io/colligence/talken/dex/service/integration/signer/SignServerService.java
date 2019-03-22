e3fdde72b6501c3dc9d82a8e3d3e65cc85ddc536
zefyr <zefyr@zezz.com>
2019-03-22 13:36:37 +0900

    signserver auth fix

--- src/main/java/io/colligence/talken/dex/service/integration/signer/SignServerService.java
+++ src/main/java/io/colligence/talken/dex/service/integration/signer/SignServerService.java
@@ -55,10 +55,10 @@ public class SignServerService extends AbstractRestApiService {
 		introduceUrl = dexSettings.getSignServer().getAddr() + "/introduce";
 		answerUrl = dexSettings.getSignServer().getAddr() + "/answer";
 
-		token = retriveAccessToken();
+		updateAccessToken();
 	}
 
-	private String retriveAccessToken() {
+	private void updateAccessToken() {
 		try {
 			String privateKey = dexSettings.getSignServer().getAppKey();
 			SignServerIntroduceRequest request = new SignServerIntroduceRequest();
@@ -68,7 +68,7 @@ public class SignServerService extends AbstractRestApiService {
 
 			if(!introResult.isSuccess()) {
 				logger.error("Cannot get signServer Token : {}, {}, {}", introResult.getResponseCode(), introResult.getErrorCode(), introResult.getErrorMessage());
-				return null;
+				return;
 			}
 
 			byte[] question = Base64.getDecoder().decode(introResult.getData().getData().getQuestion());
@@ -85,34 +85,42 @@ public class SignServerService extends AbstractRestApiService {
 
 			if(!answerResult.isSuccess()) {
 				logger.error("Cannot get signServer Token : {}, {}, {}", answerResult.getResponseCode(), answerResult.getErrorCode(), answerResult.getErrorMessage());
-				return null;
+				return;
 			}
 
 			logger.info("Received new signServer JWT Token");
-//			logger.debug("{}", answerResult.getData().getData().getWelcomePresent());
-
-			return answerResult.getData().getData().getWelcomePresent();
+			token = answerResult.getData().getData().getWelcomePresent();
 		} catch(Exception e) {
 			logger.exception(e);
 		}
-
-		return null;
 	}
 
-	private APIResult<SignServerSignResponse> requestSign(SignServerSignRequest request) {
+	private APIResult<SignServerSignResponse> requestSign(SignServerSignRequest request) throws SigningException {
+		if(token == null) updateAccessToken();
+
 		if(token == null) {
-			token = retriveAccessToken();
-			if(token == null) {
-				return null;
-			}
+			throw new SigningException(request.getAddress(), "Cannot request sign, access token is null");
 		}
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAuthorization("Bearer " + token);
+		APIResult<SignServerSignResponse> result = requestPost(signingUrl, headers, request, SignServerSignResponse.class);
+
+		if(result.isSuccess()) return result;
+
+		// if failed code is not "UNAUTHORIZED", just return error result
+		if(result.getResponseCode() != HttpStatusCodes.STATUS_CODE_UNAUTHORIZED) {
+			return result;
+		}
+
+		// case of unauthorized result, mostly case of token expiration
+		// try update access token and send request again
+		updateAccessToken();
+		headers.setAuthorization("Bearer " + token);
 		return requestPost(signingUrl, headers, request, SignServerSignResponse.class);
 	}
 
-	public String getAddressKeyID(String address) {
+	private String getAddressKeyID(String address) {
 		if(!keymap.containsKey("XLM:" + address))
 			keymap = vaultSecretReader.readSecret("signserver-keymap", SignServerKeyMap.class);
 		return keymap.get("XLM:" + address);
@@ -133,18 +141,9 @@ public class SignServerService extends AbstractRestApiService {
 		ssReq.setData(ByteArrayUtils.toHexString(tx.hash()));
 
 		APIResult<SignServerSignResponse> signResult = requestSign(ssReq);
-		if(signResult != null) {
-			// try once again for sure (in case of access token is expired)
-			if(!signResult.isSuccess() && signResult.getResponseCode() == HttpStatusCodes.STATUS_CODE_UNAUTHORIZED) {
-				signResult = requestSign(ssReq);
-			}
-		}
-		if(signResult == null) {
-			throw new SigningException(accountId, "Cannot use ss");
-		}
 
 		if(!signResult.isSuccess()) {
-			throw new SigningException(accountId, signResult.getErrorMessage());
+			throw new SigningException(accountId, signResult.getErrorCode() + " : " + signResult.getErrorMessage());
 		}
 
 		byte[] sigBytes = ByteArrayUtils.fromHexString(signResult.getData().getData().getSignature());
