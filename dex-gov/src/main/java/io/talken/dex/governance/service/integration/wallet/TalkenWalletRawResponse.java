e98f51391a6942ba612393ca88ef4dea1727c002
ZeFyr <zefyr@zezz.com>
2019-08-29 15:57:24 +0900

    ignore wallet server response http code

--- dex-gov/src/main/java/io/talken/dex/governance/service/integration/wallet/TalkenWalletRawResponse.java
+++ dex-gov/src/main/java/io/talken/dex/governance/service/integration/wallet/TalkenWalletRawResponse.java
@@ -1,12 +1,11 @@
 package io.talken.dex.governance.service.integration.wallet;
 
-import io.talken.common.util.integration.rest.RestApiResponseInterface;
 import io.talken.common.util.integration.rest.StringRestApiResponse;
 
 public class TalkenWalletRawResponse extends StringRestApiResponse {
 	@Override
 	public boolean checkHttpResponse(int httpStatus) {
-		return RestApiResponseInterface.standardHttpSuccessCheck(httpStatus);
+		return true; // always true
 	}
 
 	@Override
