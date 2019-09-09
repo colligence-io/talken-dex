910c2237758019369b71d5ee6feaf8c572e53fdf
ZeFyr <zefyr@zezz.com>
2019-09-09 14:18:29 +0900

    anchor request enabled

--- dex-api/src/main/java/io/talken/dex/api/controller/mapper/AnchorController.java
+++ dex-api/src/main/java/io/talken/dex/api/controller/mapper/AnchorController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 
-// FIXME : disabled before applying stellar-sdk 0.9.0
 @RestController
 public class AnchorController {
 	private static final PrefixedLogger logger = PrefixedLogger.getLogger(AnchorController.class);
@@ -32,36 +31,36 @@ public class AnchorController {
 
 	@Autowired
 	private AuthInfo authInfo;
-//
-//	@AuthRequired
-//	@RequestMapping(value = RequestMappings.ANCHOR_TASK, method = RequestMethod.POST)
-//	public DexResponse<AnchorResult> anchor(@RequestBody AnchorRequest postBody) throws TalkenException {
-//		DTOValidator.validate(postBody);
-//		return DexResponse.buildResponse(anchorService.anchor(authInfo.getUserId(), postBody.getFrom(), postBody.getStellar(), postBody.getSymbol(), postBody.getAmount(), postBody));
-//	}
-//
-//	@RequestMapping(value = RequestMappings.ANCHOR_TASK_DEXKEY, method = RequestMethod.POST)
-//	public DexResponse<DexKeyResult> anchorDexKey(@RequestBody DexKeyRequest postBody) throws TalkenException {
-//		DTOValidator.validate(postBody);
-//		return DexResponse.buildResponse(anchorService.anchorDexKey(postBody.getUserId(), postBody.getTaskId(), postBody.getTransId(), postBody.getSignature()));
-//	}
-//
-//	@AuthRequired
-//	@RequestMapping(value = RequestMappings.DEANCHOR_TASK, method = RequestMethod.POST)
-//	public DexResponse<DeanchorResult> deanchor(@RequestBody DeanchorRequest postBody) throws TalkenException {
-//		DTOValidator.validate(postBody);
-//		return DexResponse.buildResponse(anchorService.deanchor(authInfo.getUserId(), postBody.getPrivateWalletAddress(), postBody.getTradeWalletAddress(), postBody.getAssetCode(), postBody.getAmount(), Optional.ofNullable(postBody.getFeeByTalk()).orElse(false)));
-//	}
-//
-//	@RequestMapping(value = RequestMappings.DEANCHOR_TASK_DEXKEY, method = RequestMethod.POST)
-//	public DexResponse<DexKeyResult> deanchorDexKey(@RequestBody DexKeyRequest postBody) throws TalkenException {
-//		DTOValidator.validate(postBody);
-//		return DexResponse.buildResponse(anchorService.deanchorDexKey(postBody.getUserId(), postBody.getTaskId(), postBody.getTransId(), postBody.getSignature()));
-//	}
-//
-//	@RequestMapping(value = RequestMappings.DEANCHOR_FEE, method = RequestMethod.POST)
-//	public DexResponse<CalculateFeeResult> deanchorFee(@RequestBody DeanchorRequest postBody) throws TalkenException {
-//		DTOValidator.validate(postBody);
-//		return DexResponse.buildResponse(feeService.calculateDeanchorFee(postBody.getAssetCode(), postBody.getAmount(), postBody.getFeeByTalk()));
-//	}
+
+	@AuthRequired
+	@RequestMapping(value = RequestMappings.ANCHOR_TASK, method = RequestMethod.POST)
+	public DexResponse<AnchorResult> anchor(@RequestBody AnchorRequest postBody) throws TalkenException {
+		DTOValidator.validate(postBody);
+		return DexResponse.buildResponse(anchorService.anchor(authInfo.getUserId(), postBody.getFrom(), postBody.getStellar(), postBody.getSymbol(), postBody.getAmount(), postBody));
+	}
+
+	@RequestMapping(value = RequestMappings.ANCHOR_TASK_DEXKEY, method = RequestMethod.POST)
+	public DexResponse<DexKeyResult> anchorDexKey(@RequestBody DexKeyRequest postBody) throws TalkenException {
+		DTOValidator.validate(postBody);
+		return DexResponse.buildResponse(anchorService.anchorDexKey(postBody.getUserId(), postBody.getTaskId(), postBody.getTransId(), postBody.getSignature()));
+	}
+
+	@AuthRequired
+	@RequestMapping(value = RequestMappings.DEANCHOR_TASK, method = RequestMethod.POST)
+	public DexResponse<DeanchorResult> deanchor(@RequestBody DeanchorRequest postBody) throws TalkenException {
+		DTOValidator.validate(postBody);
+		return DexResponse.buildResponse(anchorService.deanchor(authInfo.getUserId(), postBody.getPrivateWalletAddress(), postBody.getTradeWalletAddress(), postBody.getAssetCode(), postBody.getAmount(), Optional.ofNullable(postBody.getFeeByTalk()).orElse(false)));
+	}
+
+	@RequestMapping(value = RequestMappings.DEANCHOR_TASK_DEXKEY, method = RequestMethod.POST)
+	public DexResponse<DexKeyResult> deanchorDexKey(@RequestBody DexKeyRequest postBody) throws TalkenException {
+		DTOValidator.validate(postBody);
+		return DexResponse.buildResponse(anchorService.deanchorDexKey(postBody.getUserId(), postBody.getTaskId(), postBody.getTransId(), postBody.getSignature()));
+	}
+
+	@RequestMapping(value = RequestMappings.DEANCHOR_FEE, method = RequestMethod.POST)
+	public DexResponse<CalculateFeeResult> deanchorFee(@RequestBody DeanchorRequest postBody) throws TalkenException {
+		DTOValidator.validate(postBody);
+		return DexResponse.buildResponse(feeService.calculateDeanchorFee(postBody.getAssetCode(), postBody.getAmount(), postBody.getFeeByTalk()));
+	}
 }
