dc475164e7ce1432c4a87285987160487f1b98f3
noir2k <noir2k@gmail.com>
2020-11-26 20:53:10 +0900

    fix min bal

--- dex-api/src/main/java/io/talken/dex/api/service/WalletService.java
+++ dex-api/src/main/java/io/talken/dex/api/service/WalletService.java
@@ -66,7 +66,7 @@ public class WalletService {
 	@Autowired
 	private RedisTemplate<String, Object> redisTemplate;
 
-	private final BigDecimal MINIMUM_LUK_FOR_TRANSFER = BigDecimal.valueOf(1.5);
+	private final BigDecimal MINIMUM_LUK_FOR_TRANSFER = BigDecimal.valueOf(2.381);
 	private final int LUK_TRANSFER_PENDING_TIME = 5;
 
 	/**
