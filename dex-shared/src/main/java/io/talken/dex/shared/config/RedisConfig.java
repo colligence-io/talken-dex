fa0bdfa482282157cfcea38aee7c5280b07bfaa2
noir2k <noir2k@gmail.com>
2021-04-29 18:54:07 +0900

    add redisTemplate serializer for hashKeyValue

--- dex-shared/src/main/java/io/talken/dex/shared/config/RedisConfig.java
+++ dex-shared/src/main/java/io/talken/dex/shared/config/RedisConfig.java
@@ -35,6 +35,8 @@ public class RedisConfig {
 		redisTemplate.setConnectionFactory(redisConnectionFactory());
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
 		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 		return redisTemplate;
 	}
 
